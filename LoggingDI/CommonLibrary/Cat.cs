0bb6ad2179c450e5fe59f4d0db28ef521dbb01af
demitry <dpoluektov@gmail.com>
2023-05-01 15:31:37 +0300

    LoggingDI - Common Library - Microsoft.Extensions.Logging.Abstractions

---
+++ LoggingDI/CommonLibrary/Cat.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CommonLibrary
+{
+    public class Cat
+    {
+        private readonly ILogger<Cat> _logger;
+
+        public Cat(ILogger<Cat> logger)
+        {
+            _logger = logger;
+        }
+
+        public string MakeSound()
+        {
+            _logger.LogInformation("Cat is going to meow now...");
+            return "Meow!";
+        }
+    }
+}
