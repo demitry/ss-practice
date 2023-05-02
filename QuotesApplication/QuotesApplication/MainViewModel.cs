c0052fb39318d981ca8df71d2d6dee11c37c975f
demitry <dpoluektov@gmail.com>
2023-05-02 19:19:48 +0300

    fix binding with INotifyPropertyChanged

--- QuotesApplication/QuotesApplication/MainViewModel.cs
+++ QuotesApplication/QuotesApplication/MainViewModel.cs
@@ -1,18 +1,31 @@
-﻿using System.Windows.Input;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace QuotesApplication
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
         public ICommand LoginCommand { get; set; }
 
         private string _username;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null )
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public string UserName
         {
             get { return _username; }
-            set { _username = value; }
+            set 
+            { 
+                _username = value;
+                OnPropertyChanged(nameof(UserName));
+            }
         }
 
 
