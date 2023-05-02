09c2f95e42b1b35e166372cf6828065fa72706de
demitry <dpoluektov@gmail.com>
2023-05-02 18:28:26 +0300

    Commanding (instead of code behind events)

--- QuotesApplication/QuotesApplication/MainPage.xaml.cs
+++ QuotesApplication/QuotesApplication/MainPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace QuotesApplication
 {
@@ -13,6 +7,7 @@ namespace QuotesApplication
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
         }
     }
 }
---
+++ QuotesApplication/QuotesApplication/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace QuotesApplication
+{
+    public class MainViewModel
+    {
+        public ICommand LoginCommand { get; set; }
+
+        private string _username;
+
+        public string UserName
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+
+        public MainViewModel() 
+        {
+            LoginCommand = new Command(Login);            
+        }
+
+        private void Login()
+        {
+            App.Current.MainPage.DisplayAlert("Hi", _username, "Cancel");
+        }
+    }
+}
